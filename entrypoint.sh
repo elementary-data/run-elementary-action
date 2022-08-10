e3d027db70fdb921580d2aea62d4b27d84462cb7
Elon Gliksberg <elongliks@gmail.com>
2022-08-10 18:28:34 +0300

    Accepting the edr-command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,13 @@
 
 WAREHOUSE_TYPE=$1
 PROFILES_YML=$2
-ELEMENTARY_CONF_YML=$3
-EDR_COMMAND=$4
+EDR_COMMAND=$3
 
 echo "Installing Elementary with '$WAREHOUSE_TYPE' adapter."
 pip3 install "elementary-data[$WAREHOUSE_TYPE]"
 
 echo "Configuring Dbt profile."
 mkdir -p ~/.dbt
-echo "$BIGQUERY_KEYFILE" > ~/.dbt/bigquery_keyfile.json
 echo "$PROFILES_YML" > ~/.dbt/profiles.yml
 
 echo "Running the edr command."
