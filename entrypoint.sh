2b61abf36553bff59ad36b8a340681b678610dd0
Elon Gliksberg <elongliks@gmail.com>
2022-08-10 18:23:28 +0300

    Accepting the edr-command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,15 @@
 WAREHOUSE_TYPE=$1
 PROFILES_YML=$2
 ELEMENTARY_CONF_YML=$3
-SHOULD_RUN_MONITOR=$4
-SHOULD_RUN_SEND_REPORT=$5
+EDR_COMMAND=$4
 
 echo "Installing Elementary with '$WAREHOUSE_TYPE' adapter."
 pip3 install "elementary-data[$WAREHOUSE_TYPE]"
 
-echo "Configuring Elementary."
-mkdir -p ~/.edr
-echo "$ELEMENTARY_CONF_YML" > ~/.edr/config.yml
-
-echo "Configuring Dbt."
+echo "Configuring Dbt profile."
 mkdir -p ~/.dbt
 echo "$BIGQUERY_KEYFILE" > ~/.dbt/bigquery_keyfile.json
 echo "$PROFILES_YML" > ~/.dbt/profiles.yml
 
-if [ "$SHOULD_RUN_MONITOR" = "true" ]; then
-    edr monitor
-fi
-
-if [ "$SHOULD_RUN_SEND_REPORT" = "true" ]; then
-    edr monitor send-report
-fi
+echo "Running the edr command."
+$EDR_COMMAND
