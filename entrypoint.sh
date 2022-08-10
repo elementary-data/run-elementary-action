13558b0d82207e9c47e22f5af6c29480d103a796
Elon Gliksberg <elongliks@gmail.com>
2022-08-10 17:50:15 +0300

    Using normal pip repository.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,7 @@ SHOULD_RUN_MONITOR=$4
 SHOULD_RUN_SEND_REPORT=$5
 
 echo "Installing Elementary with '$WAREHOUSE_TYPE' adapter."
-# pip3 install "elementary-data[$WAREHOUSE_TYPE]"
-pip3 install "git+https://github.com/elementary-data/elementary.git@master"
+pip3 install "elementary-data[$WAREHOUSE_TYPE]"
 
 echo "Configuring Elementary."
 mkdir -p ~/.edr
